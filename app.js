2fdc0b475b43ed002dbe0e0d9a6fc0d1264dcd4b
정용재 <jeong-yongjae@jeong-yongjaeui-MacBookAir.local>
2024-05-28 22:54:48 +0900

    asd

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose.connect(mongoURI)
+  .then(() => console.log('디비 연결'))
+  .catch((err) => console.log('디비연결 에러'));
+
+app.listen(process.env.PORT || 5055, () => {
+  console.log('server on')
+});
+
+
+
